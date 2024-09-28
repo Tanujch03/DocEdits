49b5c953c9184f01be14e44f98c55e809a58334d
Tanujch03 <tanujchaganti@gmail.com>
2024-09-29 00:44:58 +0530

    signin and signup done

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
+import { SignedOut,SignedIn,SignInButton,UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 function page() {
@@ -9,7 +10,12 @@ function page() {
         <div className='flex w-fit items-center justify-center gap-2'>
           <p className='document-title'>This is a document</p>
         </div>
-        
+        <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
       </Header>
       <Editor />
     </div>
