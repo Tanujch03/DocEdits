d6d29dbc94c0d08ea6396e14015f99126823709c
Tanujch03 <tanujchaganti@gmail.com>
2024-09-28 16:36:22 +0530

    header added

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,16 @@
 import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
 import React from 'react'
 
 function page() {
   return (
     <div>
+      <Header  >
+        <div className='flex w-fit items-center justify-center gap-2'>
+          <p className='document-title'>This is a document</p>
+        </div>
+        
+      </Header>
       <Editor />
     </div>
   )
