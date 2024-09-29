8c938746b8e35254b8832e9de73ae9eb819f349e
Tanujch03 <tanujchaganti@gmail.com>
2024-09-29 12:06:59 +0530

    start document working

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 
 import Image from 'next/image'
-const Header = ({children}:HeaderProps) => {
+import { cn } from '@/lib/utils'
+const Header = ({children,className}:HeaderProps) => {
   return (
-    <div className='header'>
+    <div className={cn("header",className)}>
       <Link href='/'  className='md:flex-1'>
       <Image src="/assets/icons/logo.svg" alt='Logo with name' width={120} height={32} className='hidden md:block'/>
       <Image src="/assets/icons/logo-icon.svg" alt='Logo' width={120} height={32} className='mr-2 md:hidden'/>
